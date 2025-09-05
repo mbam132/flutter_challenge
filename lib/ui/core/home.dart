070e838e8dde88fdced2c4a49ea12615f002363a
Francisco <fguzman53000@gmail.com>
2025-09-05 11:15:46 -0400

    home screen

---
+++ lib/ui/core/home.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_challenge/ui/character/screen.dart';
+import 'package:flutter_app_challenge/ui/episodes/screen.dart';
+import '../characters/screen.dart'; // Import the characters screen
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Desafío Flutter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 250,
+              child: ClipRRect(
+                child: Image.asset(
+                  'assets/images/download.jpeg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 6),
+              Wrap(
+                alignment: WrapAlignment.center,
+                spacing: 12,
+                runSpacing: 12,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFE9E8E4),
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => const CharactersScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Ir a personajes',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFE9E8E4),
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => const EpisodesScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Ir a episodios',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFE9E8E4),
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => const CharacterScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Buscar personaje',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
